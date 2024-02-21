39e0f0e58bc696da96512037b5acfecb5e6d7a22
SabienNguyen <sabien.q.nguyen@gmail.com>
2024-02-21 09:02:44 -0800

    change webscraper to pokemon product scraper

--- main.go
+++ main.go
@@ -1,19 +1,85 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly"
 )
 
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
 func main() {
+	var pokemonProducts []PokemonProduct
 	c := colly.NewCollector()
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		//retrieve url link
-		link := e.Attr("href")
-		fmt.Println(link)
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL)
 	})
 
-	c.Visit("https://google.com")
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong: ", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Page visited: ", r.Request.URL)
+	})
+
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+		fmt.Printf("%v\n", e.Attr("href"))
+	})
+
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		//extract all li elements of product class
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, " scraped!")
+	})
+
+	c.Visit("https://scrapeme.live/shop/")
+
+	file, err := os.Create("products.csv")
+
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+
+	writer.Write(headers)
+
+	for _, pokemonProduct := range pokemonProducts {
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+
+		writer.Write(record)
+	}
+
+	defer writer.Flush()
+
 }
